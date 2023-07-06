3cce8643db3f886d579a5786255b2a7e514892c2
pratyakshroy47 <133042425+pratyakshroy47@users.noreply.github.com>
2023-07-06 23:15:51 +0530

    Time: 19 ms (34.25%), Space: 14.2 MB (39.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
 
-    void helper(vector<vector<int>>& res, vector<int>& curr, vector<int>& candidates, int& target, int curr_sum, int i){
-        if(curr_sum == target){
-            res.push_back(curr);
-            return;
+    //Recursion method: 
+
+    // void helper(vector<vector<int>>& res, vector<int>& curr, vector<int>& candidates, int& target, int curr_sum, int i){
+    //     if(curr_sum == target){
+    //         res.push_back(curr);
+    //         return;
+    //     }
+
+    //     if(i>=candidates.size() || curr_sum>target){
+    //         return;
+    //     }
+
+    //     curr.push_back(candidates[i]);
+    //     helper(res, curr, candidates, target, curr_sum + candidates[i], i);
+
+    //     curr.pop_back();
+
+    //     if(i+1 < candidates.size()){
+    //         helper(res, curr, candidates, target, curr_sum, i+1);
+    //     }
+    // }
+    // vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    //     vector<vector<int>> res;
+    //     vector<int> curr;
+    //     helper(res, curr, candidates, target, 0, 0);
+
+    //     return res;
+    // }
+
+    //DP method:
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target){
+        vector<vector<vector<int>>> dp(target+1);
+        dp[0] = {{}};
+        for(int &i:candidates){
+            for(int j=i; j<=target; j++){
+                for(auto v:dp[j-i]){
+                    v.push_back(i);
+                    dp[j].push_back(v);
+                }
+            }
         }
-
-        if(i>=candidates.size() || curr_sum>target){
-            return;
-        }
-
-        curr.push_back(candidates[i]);
-        helper(res, curr, candidates, target, curr_sum + candidates[i], i);
-
-        curr.pop_back();
-
-        if(i+1 < candidates.size()){
-            helper(res, curr, candidates, target, curr_sum, i+1);
-        }
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> curr;
-        helper(res, curr, candidates, target, 0, 0);
-
-        return res;
+        return dp[target];
     }
 };
