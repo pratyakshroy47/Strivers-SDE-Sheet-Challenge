410398b4737c51d0e60dd205af80e8ff425e4d21
Pratyaksh Roy <133042425+pratyakshroy47@users.noreply.github.com>
2024-01-07 23:40:33 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+      bool isPossible(int arr[],int n, int k, int mid){
+        int sum = 0;
+        int part = 1;
+        for(int i =0; i < n; i++){
+            if(sum + arr[i] <= mid){
+                sum += arr[i];
+            }
+            else{
+                part++;
+                sum = arr[i];
+                
+                if(arr[i] > mid || part > k){
+                    return false;
+                }
+                
+            }
+        }
+        
+        return true;
+    }
+    int splitArray(int arr[] ,int N, int K) {
+        
+        int low = *min_element(arr, arr + N), high = accumulate(arr,arr+N,0);
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high -low) /2;
+            
+            if(isPossible(arr,N,K, mid)){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        
+        cin>>N>>K;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.splitArray(arr,N,K);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
